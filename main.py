94bcf6cad2f8c59d2199d0e5a9bbf914dde7a9b5
Artikhog <55949428+Artikhog@users.noreply.github.com>
2022-05-01 23:05:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,47 +1,84 @@
-#!/usr/bin/env python3
-from ev3dev.ev3 import *
 import math
-from math import *
-import time
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as patches
 
 
-motorL = LargeMotor('outB')
-motorR = LargeMotor('outC')
-gyro_port =
+def selectFunction():
+    global f
+    if c == 0:
+        f = lambda x: 5**x
+    elif c == 1:
+        f = lambda x: 5**(x + 0.5 * di)
+    elif c == 2:
+        f = lambda x: 5**(x + di)
+    else:
+        print("Ну ты и душнила. По умолчанию введено среднее оснащение. ")
+        f = lambda x: 5 ** (x + 0.5 * di)
 
-k1 = 0.487
-k2 = 0.024
-k3 = 0.153
-k4 = 0
 
-start_time = time.time()
-motorL.position = motorR.position = t = 0
+def summation():
+    integralsum = 0
+    for i in range(n):
+        integralsum += f(s[i]) * di
+        ax.add_patch(patches.Rectangle((s[i], 0), di, f(s[i]), edgecolor='blue', fill=False))
+    print(integralsum)
+    plt.title("Значение интегральной суммы = " + str(round(integralsum, 5)))
+    plt.show()
 
-segway_angle = 0
-segway_speed = 0
-wheel_angle = 0
-last_wheel_angle = 0
-wheel_speed = 0
-u = 0
 
-SetSensorHTGyro(gyro_port)
+def plotFunction():
+    global ax, fig
+    fig, ax = plt.subplots()
+    x = np.linspace(0, math.pi)
+    y = [5 ** i for i in x]
+    ax.grid()
+    plt.plot(x, y, 'r', label='line one', linewidth=1)
+    plt.xlabel("ось X")
+    plt.ylabel("ось Y")
 
-while True:
-    dt = time.time() - time_now
-    time_now = time.time() - start_time
-    segway_speed = (SensorHTGyro(gyro_port))
-    segway_angle += segway_speed * dt
 
-    last_wheel_angle = wheel_angle
-    wheel_angle = (motorR.position + motorL.position) / 2
-    wheel_speed = (wheel_angle - last_wheel_angle) / dt
+def inputDivision():
+    global n
+    try:
+        print("Введите количество точек разбиения:")
+        n = int(input())
+        if n < 0:
+            print("Количество точек разбиения не может быть отрицательным. ")
+            inputDivision()
+    except:
+        n = 20
+        print("Ну ты и душнила. Введено разбиение по умолчанию. (n=20)")
 
-    u = k1 * segway_angle + k2 * wheel_speed + k3 * segway_speed + k4 * wheel_angle
 
-    if u > 100:
-        u = 100
-    elif u < -100:
-        u = -100
+def inputEquipment():
+    global c
+    try:
+        print("Введите тип оснащения: 0-левое оснащение, 1-среднее оснащение, 2-правое оснащение:")
+        c = int(input())
+    except:
+        c = 1
+        print("Ну ты и душнила. По умолчанию введено среднее оснащение.")
 
-    motorL.run_direct(duty_cycle_sp=u)
-    motorR.run_direct(duty_cycle_sp=u)
+
+def createDivision():
+    global di, s
+    di = math.pi / n
+    s = [i * di for i in range(n)]
+
+
+inputDivision()
+inputEquipment()
+createDivision()
+selectFunction()
+plotFunction()
+summation()
+
+
+
+
+
+
+
+
+
